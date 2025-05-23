2c96273f8110850739844d61493b48ea69caabbb
dicarne <dicarne@zhishudali.ink>
2025-05-23 09:21:15 +0800

    [ext] 支持ai扩展库，可以调用openai库

--- ext/ai/src/index.ts
+++ ext/ai/src/index.ts
@@ -1,11 +1,22 @@
-import { env } from "cloudflare:workers";
 
-export default function () {
-	return {
-		async invoke(url: string, init: any) {
-			return "hello text";
+import OpenAI from "openai";
+
+const res = {
+	async createOpenAIClient(API_KEY: string, BASE_URL: string, MODEL: string) {
+		return async (messages: any[]) => {
+			const openai = new OpenAI({
+				apiKey: API_KEY,
+				baseURL: BASE_URL,
+			})
+			let r = await openai.chat.completions.create({
+				messages: messages,
+				model: MODEL,
+			})
+			return r.choices[0].message.content
 		}
-	}
-
+	},
 }
 
+export default function () {
+	return res
+}
