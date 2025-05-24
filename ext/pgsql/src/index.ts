e298c2ce5f25d23393df6cd79a34378f135510ca
dicarne <dicarne@zhishudali.ink>
2025-05-24 19:41:36 +0800

    [feat] 初步实现sql的功能

--- ext/pgsql/src/index.ts
+++ ext/pgsql/src/index.ts
@@ -3,55 +3,53 @@ import { Client } from "pg";
 
 import { WorkerEntrypoint, env } from 'cloudflare:workers'
 
+const eenv = env as unknown as any
 function config() {
-    // 从环境变量中获取配置信息
-    let cfg = {
-        "user": env.USER,
-        "host": env.HOST,
-        "port": env.PORT,
-        "password": env.PASSWORD,
-        "database": env.DATABASE,
-    }
+	// 从环境变量中获取配置信息
+	let cfg = {
+		"user": eenv.USER,
+		"host": eenv.HOST,
+		"port": eenv.PORT,
+		"password": eenv.PASSWORD,
+		"database": eenv.DATABASE,
+	}
 
-    // 遍历配置对象，检查每个属性是否为空
-    for (const [key, value] of Object.entries(cfg)) {
-        if (!value) {
-            throw new Error(`Environment variable ${key.toUpperCase()} is missing or empty`);
-        }
-    }
+	// 遍历配置对象，检查每个属性是否为空
+	for (const [key, value] of Object.entries(cfg)) {
+		if (!value) {
+			throw new Error(`Environment variable ${key.toUpperCase()} is missing or empty`);
+		}
+	}
 
-    return cfg
+	return cfg
 }
 
-export default class PGSQL extends WorkerEntrypoint {
+const cfg = config()
 
+export default class PGSQL extends WorkerEntrypoint {
+	client = new Client({
+		user: cfg.user,
+		host: cfg.host,
+		database: cfg.database,
+		password: cfg.password,
+		port: Number(cfg.port),
+	});
 	constructor(ctx: any, env: any) {
 		super(ctx, env)
 	}
-	async call() {
-		let cfg = config()
-		// Create a new client instance for each request.
-		const client = new Client({
-			user: cfg.user,
-			host: cfg.host,
-			database: cfg.database,
-			password: cfg.password,
-			port: Number(cfg.port),
-		});
-
-		// Connect to the database
-		await client.connect();
-		console.log("Connected to PostgreSQL database");
-
-		// Perform a simple query
-		const result = await client.query("SELECT * FROM pg_tables");
-		
+	async start() {
+		await this.client.connect()
+	}
+	async end() {
+		await this.client.end()
+	}
+	async query(sql: string, params: any[] = []) {
+		const result = await this.client.query(sql, params)
 		return {
 			rows: result.rows,
 			rowCount: result.rowCount,
 			command: result.command,
 			oid: result.oid,
 		}
-
 	}
 }
