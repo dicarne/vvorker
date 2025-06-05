e7823fca31e1cccaa02eb7b43d354a53cf9cd021
dicarne <dicarne@zhishudali.ink>
2025-06-05 17:50:50 +0800

    [feat] 增加奇妙ai流式接口支持

--- ext/ai/src/index.ts
+++ ext/ai/src/index.ts
@@ -3,13 +3,11 @@ import OpenAI from "openai";
 import { WorkerEntrypoint } from 'cloudflare:workers'
 
 export default class OpenAIWorker extends WorkerEntrypoint {
-	count: number
 	API_KEY: string
 	BASE_URL: string
 	MODEL: string
 	constructor(ctx: any, env: any) {
 		super(ctx, env)
-		this.count = 0;
 		this.API_KEY = env.API_KEY;
 		this.BASE_URL = env.BASE_URL;
 		this.MODEL = env.MODEL;
@@ -24,5 +22,44 @@ export default class OpenAIWorker extends WorkerEntrypoint {
 			model: this.MODEL,
 		})
 		return r.choices[0].message.content
+	}
+	async chat(messages: any[]) {
+		const openai = new OpenAI({
+			apiKey: this.API_KEY,
+			baseURL: this.BASE_URL,
+		})
+		let r = await openai.chat.completions.create({
+			messages: messages,
+			model: this.MODEL,
+		})
+		return r
+	}
+	async chatStream(messages: any[]) {
+		const openai = new OpenAI({
+			apiKey: this.API_KEY,
+			baseURL: this.BASE_URL,
+		})
+		let r = await openai.chat.completions.create({
+			messages: messages,
+			model: this.MODEL,
+			stream: true,
+		})
+
+		let chatStreamIterator: AsyncIterator<any> = r[Symbol.asyncIterator]();
+		let err: any = null;
+		return {
+
+			async next() {
+				try {
+					if (chatStreamIterator) {
+						return chatStreamIterator.next();
+					}
+				} catch (error) {
+					err = error;
+				} finally {
+					return { done: true, err };
+				}
+			}
+		}
 	}
 }
