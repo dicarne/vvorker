78ce8e3e3fcfa8c797674807075bc7542e90ebd7
dicarne <dicarne@zhishudali.ink>
2025-06-10 17:53:35 +0800

    [feat] 优化kv

--- ext/kv/src/index.ts
+++ ext/kv/src/index.ts
@@ -1,38 +1,61 @@
 // filepath: src/index.ts
 import { createClient } from "redis";
 
-import { WorkerEntrypoint, env } from 'cloudflare:workers'
+import { RpcTarget, WorkerEntrypoint, env } from 'cloudflare:workers'
 
 const eenv = env as unknown as any
 
 eenv.RESOURCE_ID = eenv.RESOURCE_ID || ""
 const prefix = eenv.RESOURCE_ID.length > 0 ? eenv.RESOURCE_ID + ":" : ""
 
+async function client() {
+	let redis = createClient({
+		url: `redis://${eenv.HOST}:${eenv.PORT}/0`
+	})
+	await redis!.connect()
+	return redis
+}
+
+class RedisTarget extends RpcTarget {
+	constructor() {
+		super()
+
+	}
+	redis: ReturnType<typeof createClient> | null = null
+
+	async connect() {
+		this.redis = await client()
+	}
+
+	async get(key: string) {
+		return await this.redis!.get(prefix + key)
+	}
+
+	async set(key: string, value: string) {
+		return await this.redis!.set(prefix + key, value)
+	}
+
+	async del(key: string) {
+		return await this.redis!.del(prefix + key)
+	}
+
+	async keys(pattern: string) {
+		return await this.redis!.keys(prefix + pattern)
+	}
+
+	async ping() {
+		return await this.redis?.ping()
+	}
+}
+
+
 export default class Redis extends WorkerEntrypoint {
 	constructor(ctx: any, env: any) {
 		super(ctx, env)
 	}
-	async start() {
-		const redis = createClient({
-			url: `redis://${eenv.HOST}:${eenv.PORT}/0`
-		})
-		await redis.connect()
-		return {
-			end: async () => {
-				await redis.quit()
-			},
-			get: async (key: string) => {
-				return await redis.get(prefix + key)
-			},
-			set: async (key: string, value: string) => {
-				return await redis.set(prefix + key, value)
-			},
-			del: async (key: string) => {
-				return await redis.del(prefix + key)
-			},
-			keys: async (pattern: string) => {
-				return await redis.keys(prefix + pattern)
-			}
-		}
+	async client() {
+		let r = new RedisTarget()
+		await r.connect()
+		return r
 	}
 }
