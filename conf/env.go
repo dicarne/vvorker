c9da4d4cc54fb033fa6a9f848ff9a1855440f827
dicarne <dicarne@zhishudali.ink>
2025-06-11 09:33:18 +0800

    [feat] 针对多节点组网完善了文档

--- conf/env.go
+++ conf/env.go
@@ -11,28 +11,33 @@ import (
 )
 
 type AppConfig struct {
-	DBPath            string `env:"DB_PATH" env-default:"/workerd/db.sqlite"`
-	WorkerdDir        string `env:"WORKERD_DIR" env-default:"/workerd"`
-	DBType            string `env:"DB_TYPE" env-default:"sqlite"`
-	WorkerLimit       int    `env:"WORKER_LIMIT" env-default:"10000"`
-	WorkerdBinPath    string `env:"WORKERD_BIN_PATH" env-default:"/bin/workerd"`
-	WorkerPort        int    `env:"WORKER_PORT" env-default:"8080"`
-	APIPort           int    `env:"API_PORT" env-default:"8888"`
-	APIWebBaseURL     string `env:"API_WEB_BASE_URL"`
-	ListenAddr        string `env:"LISTEN_ADDR" env-default:"0.0.0.0"`
-	WorkerURLSuffix   string `env:"WORKER_URL_SUFFIX"`         // master required, e.g. .example.com. for worker show and route
-	Scheme            string `env:"SCHEME" env-default:"http"` // http, https. for public frontend show
-	CookieName        string `env:"COOKIE_NAME" env-default:"authorization"`
-	CookieAge         int    `env:"COOKIE_AGE" env-default:"86400"` // second 86400 = 1 day
-	CookieDomain      string `env:"COOKIE_DOMAIN"`                  // required, e.g. example.com
-	EnableRegister    bool   `env:"ENABLE_REGISTER" env-default:"true"`
-	AgentSecret       string `env:"AGENT_SECRET"` //	required, e.g. 123123123
-	NodeName          string `env:"NODE_NAME" env-default:"default"`
-	MasterEndpoint    string `env:"MASTER_ENDPOINT" env-default:"http://127.0.0.1:8888"` // needed for agent
-	RunMode           string `env:"RUN_MODE" env-default:"master"`                       // master, agent
-	TunnelEntryPort   int    `env:"TUNNEL_ENTRY_PORT" env-default:"10080"`               //提供http服务
-	TunnelHost        string `env:"TUNNEL_HOST" env-default:"127.0.0.1"`                 // for master usually 127.0.0.1, for agent usually master public ip
-	TunnelAPIPort     int    `env:"TUNNEL_API_PORT" env-default:"18080"`                 // 监听
+	MasterEndpoint string `env:"MASTER_ENDPOINT" env-default:"http://127.0.0.1:8888"` // needed for agent，agent需要通过该url来注册节点
+	WorkerPort     int    `env:"WORKER_PORT" env-default:"8080"`                      // 【主节点公开】提供worker服务，如 xxx.example.com:8080
+	APIPort        int    `env:"API_PORT" env-default:"8888"`                         // 【主节点公开】提供控制台服务和控制api服务
+
+	TunnelHost      string `env:"TUNNEL_HOST" env-default:"127.0.0.1"`   // for master usually 127.0.0.1, for agent usually master public ip
+	TunnelEntryPort int    `env:"TUNNEL_ENTRY_PORT" env-default:"10080"` // 【主节点内部】提供http服务，主节点用这个端口向其他节点发送请求。在主节点提供服务，子节点无法向其发送请求。
+	TunnelAPIPort   int    `env:"TUNNEL_API_PORT" env-default:"18080"`   // 【主节点公开】TUNNEL_ENTRY_PORT的frp端口，子节点通过这个端口配置每个worker的转发
+
+	WorkerURLSuffix string `env:"WORKER_URL_SUFFIX"`         // master required, e.g. .example.com. for worker show and route
+	Scheme          string `env:"SCHEME" env-default:"http"` // http, https. for public frontend show
+	NodeName        string `env:"NODE_NAME" env-default:"default"`
+	AgentSecret     string `env:"AGENT_SECRET"` //	required, e.g. 123123123
+
+	DBPath         string `env:"DB_PATH" env-default:"/workerd/db.sqlite"`
+	WorkerdDir     string `env:"WORKERD_DIR" env-default:"/workerd"`
+	DBType         string `env:"DB_TYPE" env-default:"sqlite"`
+	WorkerLimit    int    `env:"WORKER_LIMIT" env-default:"10000"`
+	WorkerdBinPath string `env:"WORKERD_BIN_PATH" env-default:"/bin/workerd"`
+
+	APIWebBaseURL  string `env:"API_WEB_BASE_URL"`
+	ListenAddr     string `env:"LISTEN_ADDR" env-default:"0.0.0.0"`
+	CookieName     string `env:"COOKIE_NAME" env-default:"authorization"`
+	CookieAge      int    `env:"COOKIE_AGE" env-default:"86400"` // second 86400 = 1 day
+	CookieDomain   string `env:"COOKIE_DOMAIN"`                  // required, e.g. example.com
+	EnableRegister bool   `env:"ENABLE_REGISTER" env-default:"true"`
+	RunMode        string `env:"RUN_MODE" env-default:"master"` // master, agent
+
 	DefaultWorkerHost string `env:"DEFAULT_WORKER_HOST" env-default:"localhost"`
 	LitefsPrimaryPort int    `env:"LITEFS_PRIMARY_PORT" env-default:"20202"`
 	LitefsBinPath     string `env:"LITEFS_BIN_PATH" env-default:"/usr/local/bin/litefs"`
