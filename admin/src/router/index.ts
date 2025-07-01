4279cf25fd6db1989900e2b91d167124fdcf381e
louhanxin <louhanxin@akey.me>
2025-07-01 18:43:37 +0800

    [feat] 顶部栏 & 菜单

--- admin/src/router/index.ts
+++ admin/src/router/index.ts
@@ -5,19 +5,23 @@ const router = createRouter({
   routes: [
     {
       path: '/login',
+      name: 'Login',
       component: () => import('@/views/Login.vue'),
     },
     {
       path: '/register',
+      name: 'Register',
       component: () => import('@/views/Register.vue')
     },
     {
       path: '/',
-      // component: () => import('@/views/Index.vue'),
+      name: 'Index',
+      component: () => import('@/views/Index.vue'),
       redirect: '/admin',
       children: [
         {
           path: '/admin',
+          name: 'Admin',
           component: () => import('@/views/Admin.vue'),
         },
       ],
