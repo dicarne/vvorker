d1e20d89cd1f7a1438040846be339547dea040df
louhanxin <louhanxin@akey.me>
2025-07-01 15:51:56 +0800

    [init]

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -7,14 +7,19 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  server: {
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+        secure: false, // 关闭 SSL证书校验
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
