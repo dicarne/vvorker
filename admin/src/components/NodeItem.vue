b8c16a6d90265e1f990fd6ea21f7eef9c55d4672
louhanxin <louhanxin@akey.me>
2025-07-05 10:13:23 +0800

    [feat] Status Page Node Delete

--- admin/src/components/NodeItem.vue
+++ admin/src/components/NodeItem.vue
@@ -3,8 +3,7 @@ import { computed } from 'vue'
 import { NCard, NButton, useMessage } from 'naive-ui'
 import VVorkerTracker from '@/components/VVorkerTracker.vue'
 import type { Node } from '@/types/nodes'
-import { syncNodes } from '@/api/nodes'
-import { error } from 'naive-ui/es/_utils/naive/warn'
+import { deleteNode, syncNodes } from '@/api/nodes'
 
 const props = defineProps<{
   node: Node
@@ -26,25 +25,37 @@ const data = computed(() => {
     return { color: '#a3be8c', tooltip: `${v}ms` }
   })
 })
-
 const sla = computed(() => {
   return parseFloat(
     ((1 - props.ping.filter((v) => v >= 500).length / props.ping.length) * 100).toFixed(2),
   )
 })
-
 const avg = computed(() => {
   const validValue = props.ping.filter((v) => v !== -1)
   return parseFloat((validValue.reduce((a, b) => a + b, 0) / validValue.length).toFixed(2))
 })
+
 const message = useMessage()
-const handleSyncNodeClick = async () => {
+
+// 同步节点（非 Default）
+const handleSyncNodeClick = async (nodeName: string) => {
   try {
-    await syncNodes(props.node.Name)
+    await syncNodes(nodeName)
     message.info('同步成功')
   } catch (error) {
     console.error('syncNodes Error', error)
     message.error('同步失败')
+  }
+}
+
+// 删除节点（非 Default）
+const handleDeleteNodeClick = async (nodeName: string) => {
+  try {
+    await deleteNode(nodeName)
+    message.info('删除成功')
+  } catch (error) {
+    console.error('deleteNode Error', error)
+    message.error('删除失败')
   }
 }
 </script>
@@ -53,7 +64,8 @@ const handleSyncNodeClick = async () => {
   <div class="node-item">
     <NCard :title="node.Name">
       <template #header-extra>
-        <NButton type="primary" secondary @click="handleSyncNodeClick">同步</NButton>
+        <NButton v-if="node.Name !== 'default'" type="primary" secondary @click="handleSyncNodeClick(node.Name)">同步</NButton>
+        <NButton v-if="node.Name !== 'default'" type="primary" secondary @click="handleDeleteNodeClick(node.Name)">删除</NButton>
       </template>
       <div class="v-item-column">{{ node.UID }}</div>
       <div class="v-item-column" style="text-align: right">
