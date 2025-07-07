c8ab08be86a1cb0c6facebb178f59053eeeee126
louhanxin <louhanxin@akey.me>
2025-07-07 18:14:26 +0800

    [feat] PGSQL & OSS

--- admin/src/views/PGSQL.vue
+++ admin/src/views/PGSQL.vue
@@ -1,5 +1,8 @@
+<script setup lang="ts">
+import ResourceList from '@/components/ResourceList.vue'
+</script>
 <template>
-  <div>
-    <h1>This is a PGSQL page</h1>
+  <div class="v-main">
+    <ResourceList rType="pgsql" />
   </div>
 </template>
