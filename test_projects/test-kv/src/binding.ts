44dd1b3b7c80a1f4613d45dc5fa2bec2914b22a1
dicarne <dicarne@zhishudali.ink>
2025-08-08 16:45:59 +0800

    [test] 测试kv写入是否正常

--- test_projects/test-kv/src/binding.ts
+++ test_projects/test-kv/src/binding.ts
@@ -1,6 +1,20 @@
 
 export interface EnvBinding {
 
-	vars: {}
+	kv: KVBinding
 
+	vars: {
+      MODE: string;
+    }
+
+}
+export interface KVBinding {
+    get(key: string): Promise<string | null>;
+    set(key: string, value: string, options?: {
+        EX?: number,
+        NX?: boolean,
+        XX?: boolean
+    } | number): Promise<number>;
+    del(key: string): Promise<void>;
+    keys(pattern: string, offset: number, size: number): Promise<string[]>;
 }
--- test_projects/test-kv/src/index.ts
+++ test_projects/test-kv/src/index.ts
@@ -1,16 +1,17 @@
 
-  import { Hono } from "hono";
-  import { EnvBinding } from "./binding";
-  import { init, useDebugEndpoint } from "@dicarne/vvorker-sdk";
-  import { env } from "cloudflare:workers";
-  init(env)
-  
-  const app = new Hono<{ Bindings: EnvBinding }>();
-  useDebugEndpoint(app)
-  
-  app.get("/", (c) => {
-    return c.text("Hello World!!!!!!!2222!!")
-  })
-  
-  export default app;  
-    
+import { Hono } from "hono";
+import { EnvBinding } from "./binding";
+import { init, useDebugEndpoint, vvbind } from "@dicarne/vvorker-sdk";
+import { env } from "cloudflare:workers";
+init(env)
+
+const app = new Hono<{ Bindings: EnvBinding }>();
+useDebugEndpoint(app)
+
+app.get("/", async (c) => {
+  const kv = vvbind(c).kv("kv")
+  await kv.set("hi", "hello")
+  return c.text((await kv.get("hi")) || "[NULL]")
+})
+
+export default app;
