e763a4fee5ba9ad2476f15fada76fa9beb3269d8
dicarne <dicarne@zhishudali.ink>
2026-01-14 09:42:37 +0800

    [feat] 增加版本号系统

--- cmd.builddocker.bat
+++ cmd.builddocker.bat
@@ -1,1 +1,4 @@
-CALL docker buildx build --platform linux/amd64,linux/arm64 -t git.cloud.zhishudali.ink/dicarne/vvorker:latest --push .
+@echo off
+set /p VERSION=<VERSION.txt
+echo Building version %VERSION%
+CALL docker buildx build --platform linux/amd64,linux/arm64 -t git.cloud.zhishudali.ink/dicarne/vvorker:%VERSION% -t git.cloud.zhishudali.ink/dicarne/vvorker:latest --push .
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"vvorker/exec"
 	kv "vvorker/ext/kv/src"
 	"vvorker/services"
@@ -12,10 +13,37 @@ import (
 //go:embed all:admin/dist/*
 var fs embed.FS
 
+//go:embed VERSION.txt
+var Version string
+
+func printBanner() {
+	banner := `
+╔═════════════════════════════════════════════════════════════════════════════════════════╗
+║                                                                                         ║
+║     ___      ___ ___      ___ ________  ________  ___  __    _______   ________         ║
+║    |\  \    /  /|\  \    /  /|\   __  \|\   __  \|\  \|\  \ |\  ___ \ |\   __  \        ║
+║    \ \  \  /  / | \  \  /  / | \  \|\  \ \  \|\  \ \  \/  /|\ \   __/|\ \  \|\  \       ║
+║     \ \  \/  / / \ \  \/  / / \ \  \\\  \ \   _  _\ \   ___  \ \  \_|/_\ \   _  _\      ║
+║      \ \    / /   \ \    / /   \ \  \\\  \ \  \\  \\ \  \\ \  \ \  \_|\ \ \  \\  \|     ║
+║       \ \__/ /     \ \__/ /     \ \_______\ \__\\ _\\ \__\\ \__\ \_______\ \__\\ _\     ║
+║        \|__|/       \|__|/       \|_______|\|__|\|__|\|__| \|__|\|_______|\|__|\|__|    ║
+║                                                                                         ║
+║                                                                                         ║
+                                      VERSION: %s                                     
+║                                       BY: Dicarne                                       ║
+║                                                                                         ║
+╚═════════════════════════════════════════════════════════════════════════════════════════╝
+`
+	fmt.Printf(banner, Version)
+	fmt.Println()
+}
+
 func main() {
 
 	logrus.SetReportCaller(true)
 	logrus.SetLevel(logrus.DebugLevel)
+	printBanner()
+	logrus.Info("vvorker version: %s", Version)
 	defer exec.ExecManager.ExitAllCmd()
 	defer kv.Close()
 
