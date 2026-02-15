ff21a30ada5123c6a78031e0074359f5df9fe7a4
dicarne <dicarne@zhishudali.ink>
2026-02-15 15:17:48 +0800

    feat: 为 TaskBinding.create 添加 name 参数并定义 MODE 变量

--- test_projects/test-task/src/binding.ts
+++ test_projects/test-task/src/binding.ts
@@ -4,13 +4,15 @@ export interface EnvBinding {
 
 	task: TaskBinding
 
-	vars: {}
+	vars: {
+      MODE: string;
+    }
 
 }
 export interface TaskBinding {
     should_exit: () => Promise<boolean>;
     log: (text: string) => Promise<void>;
     complete: () => Promise<void>;
-    create: (trace_id?: string) => Promise<string | undefined>;
+    create: (name: string, trace_id?: string) => Promise<string | undefined>;
 }
 
